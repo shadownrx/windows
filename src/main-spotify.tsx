32c635d3548d63d618d9dd0c1f93626933702e9c
shadownrx <salvadorjuars4@gmail.com>
2026-07-09 18:37:09 -0300

    new version

--- src/main-spotify.tsx
+++ src/main-spotify.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { SpotifyMiniStandalone } from './components/apps/SpotifyMini.standalone.tsx'
+import SpotifyMiniStandalone from './components/apps/SpotifyMini.standalone.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
